d51416f5ea744a9c2fd5ac956c2eb6c3ecd60595
Mikko Sulonen <mikko.sulonen@recordlydata.com>
2023-09-18 20:53:48 +0000

    Update entrypoint

--- macros/generation_macros/generate_commands.sql
+++ macros/generation_macros/generate_commands.sql
@@ -7,16 +7,6 @@
 
 {% set shell_commands %}
 ==================================================================================================
-These commands generate the folders and empty place-holder files. Run in local project with git bash.
-==================================================================================================
-cd $(git rev-parse --show-toplevel) &&
-mkdir models/staging/ &&
-mkdir models/staging/{{source_name}} &&
-touch models/staging/{{source_name}}/_{{source_name}}__sources.yml &&
-touch models/staging/{{source_name}}/_{{source_name}}_stg__models.yml &&
-mkdir snapshots/{{source_name}}
-
-==================================================================================================
 COPY TO dbt_project.yml MODELS AND SNAPSHOTS
 ==================================================================================================
 
@@ -27,13 +17,6 @@ MODELS
       staging:
         +schema: {{source_name}}_stg
         +materialized: copy_into
-      publish:
-        +transient: false
-        +schema: {{source_name}}
-        +materialized: table
-        +post-hook: {%raw%}"{{ grant_usage('ROLE_DATASET_{%endraw%}{{source_name|upper}}{%raw%}_ENV_R', schema=schema) }}"{%endraw%}
-        +grants:
-          +select: ["ROLE_DATASET_{{source_name|upper}}_{%raw%}{{ env_var('DBT_TARGET_ENV') }}{%endraw%}_R"]
 
 SNAPSHOTS:
     # ------------------
@@ -50,9 +33,6 @@ Run:
 dbt run-operation generate_source --args '{"schema_name": "SOURCE_SCHEMA_NAME", "database_name": "SOURCE_DATABASE_NAME", "generate_columns": "true", "include_descriptions": "true", "include_data_types": "true", "name": "{{source_name}}", "include_database": "true", "include_schema": "true"}' > _{{source_name}}__sources.yml &&
 mv _{{source_name}}__sources.yml models/staging/{{source_name}}/_{{source_name}}__sources.yml
 
-Save as:
-models/staging/{{source_name}}/_{{source_name}}__sources.yml
-
 ==================================================================================================
 CREATE DBT SNAPSHOTS aka PERSISTENT STAGING WITH HISTORY
 ==================================================================================================
@@ -60,40 +40,42 @@ CREATE DBT SNAPSHOTS aka PERSISTENT STAGING WITH HISTORY
 Create persistent staging:
 dbt run-operation generate_snapshot_sql --args '{"source_name": "{{source_name}}"}'
 
-Save and split generated sql to snapshot/{{source_name}}/
-grep -vwP "\d\d:\d\d" ../../log.sql | awk '/.sql/{out=$1;next} {print > out;}'
-
 Run:
 dbt snapshot --select {{source_name}}
 
 ==================================================================================================
-CREATE INTERMEDIATE LAYER
+CREATE STAGING LAYER
 ==================================================================================================
 
-Create templates:
-dbt run-operation generate_consumption_models --args '{"from_schema_name": "dbt_cloud_{{source_name}}_snapshot", "file_name_prefix": "{{source_name}}_int__", "type": "int"}'
+dbt run-operation generate_stg_models --args '{"source_name": "{{source_name}}"}'   
 
-Build only Intermediate models:
-dbt build --select {{source_name}}.intermediate
+Create yaml:
 
-Add docs and Primary Key tests:
-dbt run-operation generate_multiple_model_yaml --args '{"table_prefix": "{{source_name}}_int__%", "add_pk_test": True, "source_table_prefix": "{{source_name}}_ext", "source_name": "{{source_name}}", "limit_pk_test": true}'
+dbt run-operation generate_model_yaml
+
+==================================================================================================
+CREATE INTERMEDIATE
+==================================================================================================
+
+The place for complex transformations.
+
+If no complex transformations, but a publish model is needed,
+
+create a "select * from staging layer"
+
+so we can add tests on a good place.
+
+Create yml for tests:
 
 ==================================================================================================
 CREATE PUBLISH
 ==================================================================================================
 
 Create publish:
-dbt run-operation generate_consumption_models --args '{"from_schema_name": "{{source_name}}_history", "file_name_prefix": "{{source_name}}_publish__", "type": "publish"}'
 
-Save and split to to models/{{source_name}}/publish/:
-grep -vwP "\d\d:\d\d" ../../../log.sql | awk '/.sql/{out=$1;next} {print > out;}'
+"select * from intermediate layer"
 
-Build only Publish models:
-dbt build --select {{source_name}}.publish
-
-Create docs and PKs:
-dbt run-operation generate_multiple_model_yaml --args '{"schema_pattern": "%_{{source_name}}", "add_pk_test": True, "source_table_prefix": "{{source_name}}_ext", "source_name": "{{source_name}}", "alias_prefix" : "{{source_name}}_publish__"}'
+Create yml for tests and docs:
 
 ==================================================================================================
 BUILD FULL MODEL
